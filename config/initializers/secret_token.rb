5ef7dc16fded12d76aa4b752f96d26f58c90aae6
npotu1 <npotu1@umbc.edu>
2014-09-08 01:09:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Npotu1DemoApp::Application.config.secret_key_base = '758875e87f870ca7414b60630fa4e8bf6ad58f29b66e45ed9c7656e26f31dee3fcc9451ddffdf6fda320369f369f9e08078484ef34cdf69d55376898421e3a24'
